647c54685812eee81e4d00b70594b99f35da66dc
Sean Dwyer <ewetoo@gmail.com>
2019-09-15 22:08:07 +1000

    .gitignore:     added
    3rdp/build/GNUmakefile
    3rdp/build/arch.patch:
        Fixes for broken cpu options
    
    build.sh:
        tuned for my environment
    
    builds.md:
        build notes

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# syncterm directory is just for builds, we do the rest by hand.
 
-sudo rm -rf /opt/syncterm
+rm -rf $HOME/syncterm
 cd src/syncterm
-MAKE_CMD="make -f GNUmakefile WITHOUT_CRYPTLIB=1 PREFIX=/opt/syncterm RELEASE=1"
+# if you don't want cryptlib define WIHOUT_CRYPTLIB=1 
+# define DEBUG=1 for a debug version and remove RELEASE
+MAKE_CMD="make -f GNUmakefile PREFIX=$HOME/syncterm RELEASE=1"
 $MAKE_CMD
-sudo $MAKE_CMD install
+$MAKE_CMD install
---
+++ builds.md
@@ -0,0 +1,6 @@
+About Syncterm Builds
+=====================
+
+By default the build script will install in $HOME/syncterm but I prefer to
+copy those files to my $HOME so the installdir is just for keeping the files
+together.
